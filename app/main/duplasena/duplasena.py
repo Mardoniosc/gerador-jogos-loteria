aac92f4c4c3eb4cee19746ba49e4710a9a7ba37f
Mardonio Costa <mardoniocosta@pjc.mt.gov.br>
2022-12-16 16:23:59 -0400

    fix: Corrigido o range de numeros da dupla sena

--- app/main/duplasena/duplasena.py
+++ app/main/duplasena/duplasena.py
@@ -1,8 +1,8 @@
 from random import sample
 
+
 def duplasena_aleatorio():
-  numeros = list(range(1, 61))
-  jogo1 =  sample(numeros, 6)
-  jogo2 =  sample(numeros, 6)
-  jogo = { 'Jogos' : [sorted(jogo1), sorted(jogo2)]}
-  return jogo
+    numeros = list(range(1, 51))
+    jogo = sample(numeros, 6)
+    jogo = {'Jogo': sorted(jogo)}
+    return jogo

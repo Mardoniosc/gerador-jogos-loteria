e9877a5255b5eb81945cd1a5026d7474a9c92f3a
Mardonio Costa <Mardonio@live.com>
2022-12-17 21:58:48 -0400

    Feito a base de regras de geração de jogos

--- app/main/regras/regras.py
+++ app/main/regras/regras.py
@@ -1,16 +1,29 @@
-megasena = {
-  'duque': ''
-}
+class Regra():
+  def __init__(self, titulo, descricao):
+    self.titulo = titulo
+    self.descricao = descricao
 
-lotofacil = {
-  'regras': 'explicacao'
-}
+  def regraObjeto(self):
+    return { self.titulo : self.descricao }
+
+# ALEATORIOS
+lotofacilAleatorio = Regra('Lotofácil Aleatória', 'Números sorteados gerados através do algoritimo(random.sample)')
+duplasenaAleatorio = Regra('DuplaSena Aleatória', 'Números sorteados gerados através do algoritimo(random.sample)')
+lotomaniaAleatorio = Regra('LotoMania Aleatória', 'Números sorteados gerados através do algoritimo(random.sample)')
+megasenaAleatorio = Regra('MegaSena Aleatória', 'Números sorteados gerados através do algoritimo(random.sample)')
+quinaAleatorio = Regra('Quina Aleatória', 'Números sorteados gerados através do algoritimo(random.sample)')
 
 
-def getAllRoles():
-  return {
-    regras: [
-      megasena,
-      lotofacil
-    ]
-  }
+
+
+def regras_aleatorio():
+  listaRegrasAleatorios = []
+  listaRegrasAleatorios.append(lotofacilAleatorio.regraObjeto())
+  listaRegrasAleatorios.append(duplasenaAleatorio.regraObjeto())
+  listaRegrasAleatorios.append(lotomaniaAleatorio.regraObjeto())
+  listaRegrasAleatorios.append(megasenaAleatorio.regraObjeto())
+  listaRegrasAleatorios.append(quinaAleatorio.regraObjeto())
+  jogo = { 'Regras' : {
+    'Aleatorios': listaRegrasAleatorios
+  }}
+  return jogo
--- app/main/regras/regras_controller.py
+++ app/main/regras/regras_controller.py
@@ -1,10 +1,10 @@
 from flask_restplus import Resource, Namespace
-from app.main.regras.regras import getAllRoles
+from app.main.regras.regras import regras_aleatorio
 
-api = Namespace('Regras', description='Regras da geração de jogos')
+api = Namespace('Regras', description='Regras para geração de jogos')
 
 @api.route('/all')
-class RegrasAllController(Resource):
-  @api.response(200, "Listado regras com sucesso")
+class RegrasAleatorioController(Resource):
+  @api.response(200, "Documento De Regras gerado com sucesso!")
   def get(self):
-    return getAllRoles, 200
+      return regras_aleatorio(), 200
